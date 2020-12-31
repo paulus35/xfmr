a706de98b4218203aecffcfcbd1e1dbf1a7a1da3
paulus35 <ing.paulo.medina@gmail.com>
2020-12-31 13:29:57 -0700

    Initial commit.

---
+++ app/Http/Controllers/XfmrController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Xfmr;
+use Illuminate\Http\Request;
+
+class XfmrController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Xfmr  $xfmr
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Xfmr $xfmr)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Xfmr  $xfmr
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Xfmr $xfmr)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Xfmr  $xfmr
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Xfmr $xfmr)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Xfmr  $xfmr
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Xfmr $xfmr)
+    {
+        //
+    }
+}
---
+++ app/Models/Xfmr.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Xfmr extends Model
+{
+    use HasFactory;
+}
